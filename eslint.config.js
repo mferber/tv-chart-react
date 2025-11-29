cc6b8193326062a34aad594a50363e6bde950a1e
Matthias Ferber <matthias.ferber@gmail.com>
2025-11-29 11:11:15 -0500

    Cleaned starter project
    - Removed default project detritus
    - Added Vite-suggested React ESLint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,9 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -14,10 +15,16 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-])
+]);
