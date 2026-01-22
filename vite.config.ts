cc201c13e5a2821f5d01bc2151deead264ee0801
Matthias Ferber <matthias.ferber@gmail.com>
2026-01-22 10:18:44 -0500

    Vite config: allow access from other devices
    ...including using the local DNS name, read from the environment instead
    of hardcoded

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import os from 'os'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      os.hostname().toLowerCase()
+    ]
+  }
 })
