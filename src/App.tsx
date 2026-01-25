ad37f74f931aa69ace7d9730e808f6dde89e05f4
Matthias Ferber <matthias.ferber@gmail.com>
2026-01-25 17:12:24 -0500

    Minimal functioning context for app environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect, createContext, use } from 'react'
+import { type ReactNode } from 'react'
+
+const AppEnvironmentContext = createContext<string | null>(null)
+
+// Provides the app environment context; also carries out initial request to server
+// to find out the environment and get a CSRF protection cookie
+function AppEnvironmentContextProvider({ children }: { children: ReactNode }) {
+  const [env, setEnv] = useState<string | null>(null)
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const rsp = await fetch("/api/env");
+      const result = await rsp.text();
+      setEnv(result);
+    }, 1500);
+  }, []);
+
+  return (
+    <AppEnvironmentContext value={env}>
+      {children}
+    </AppEnvironmentContext>
+  )
+}
 
 function App() {
   return (
     <>
-      Hello
+      <h1>Here we go</h1>
+      <AppEnvironmentContextProvider>
+        <Display />
+      </AppEnvironmentContextProvider>
     </>
   )
 }
 
-export default App
+function Display() {
+  const env = use(AppEnvironmentContext)
+  return <>
+    <p>Hello: {env ?? "null"}</p>
+  </>
+
+}
+
+export default App;
