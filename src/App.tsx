35e4ec2726881d855804cf18ce7ac5883fb84da4
Matthias Ferber <matthias.ferber@gmail.com>
2026-01-25 20:08:33 -0500

    Added CurrentUserContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,55 @@ import {
   AppEnvironmentContextProvider,
   useAppEnvironmentContext,
 } from "./contexts/AppEnvironmentContext"
+import { CurrentUserContextProvider, useCurrentUserContext, type User } from "./contexts/CurrentUserContext"
 
 function App() {
   return (
     <AppEnvironmentContextProvider>
+      <CurrentUserContextProvider>
         <Main />
+      </CurrentUserContextProvider>
     </AppEnvironmentContextProvider>
+  );
 }
 
 function Main() {
   const env = useAppEnvironmentContext()
+  const {currentUser} = useCurrentUserContext()
   return (
     <>
-      <h1>Here we go</h1>
-      <AppEnvironmentContextProvider>
-        <Display />
-      </AppEnvironmentContextProvider>
+      {env === null ? (
+        <p>"No environment info available"</p>
+      ) : (
+        <div>
+          <p>Environment: {env.environment}</p>
+          <p>CSRF token: {env.csrfToken}</p>
+        </div>
+      )}
+      <hr />
+
+      {currentUser === null ? <LoginPanel /> : <MainUI />}
     </>
-  )
+  );
 }
 
-function Display() {
-  const env = use(AppEnvironmentContext)
-  return <>
-    <p>Hello: {env ?? "null"}</p>
-  </>
+function LoginPanel() {
+  const { setCurrentUser } = useCurrentUserContext()
+    return  <div>
+        <button
+          onClick={() =>
+            setCurrentUser({ email: "nobody@nowhere.com", id: "123abc" })
+          }
+        >
+          Fake login
+        </button>
+      </div>
+}
 
+function MainUI() {
+  // current user can't be null or we wouldn't be here
+  const { currentUser } = useCurrentUserContext() as User
+  return <p>User logged in as: {currentUser.email} ({currentUser.id})</p>
 }
 
 export default App;
---
+++ src/contexts/CurrentUserContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useState, use, type Dispatch, type SetStateAction, type ReactNode } from 'react'
+
+// Context that provides the email and id of the current user, if one is logged in, otherwise null.
+
+interface User {
+  email: string
+  id: string
+}
+
+interface CurrentUserContextType {
+  currentUser: User | null
+  setCurrentUser: Dispatch<SetStateAction<User | null>>
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const CurrentUserContext = createContext<CurrentUserContextType>({} as CurrentUserContextType)
+
+export function CurrentUserContextProvider({ children }: { children: ReactNode }) {
+  const [currentUser, setCurrentUser] = useState<User | null>(null)
+
+  return (
+    <CurrentUserContext value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUserContext>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useCurrentUserContext(): CurrentUserContextType {
+  const context = use(CurrentUserContext);
+  if (context === undefined) {
+    throw new Error('useCurrentUserContext must be used within CurrentUserContextProvider');
+  }
+  return context;
+}
