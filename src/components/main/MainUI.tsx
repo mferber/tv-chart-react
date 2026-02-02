aff6b9311ac5173789649dee94c142f392d7a93c
Matthias Ferber <matthias.ferber@gmail.com>
2026-02-02 18:37:31 -0500

    Custom hook to show toast for React Query error
    I prefer this approach to the official approach of setting metadata on
    the query and setting up the toast globally on the QueryClient. This
    gives me more discretion, and the custom hook is at least less awkward
    than explicitly writing a useEffect every time.

--- src/components/main/MainUI.tsx
+++ src/components/main/MainUI.tsx
@@ -3,12 +3,18 @@ import { useQuery } from "@tanstack/react-query"
 import { useCurrentUserStatusContext } from "../../contexts/CurrentUserStatusContext"
 import { LogOutLink } from "../authentication/LogOutLink"
 import { fetchShows } from "../../api/client"
+import { useQueryErrorToast } from "../../hooks"
 
 export function MainUI() {
   // User can't be null or we wouldn't be here
   const currentUser = useCurrentUserStatusContext().user!
 
   const showsQuery = useQuery({ queryKey: ["shows"], queryFn: fetchShows })
+  useQueryErrorToast(
+    showsQuery,
+    "Shows could not be loaded due to a network problem",
+  )
+
   return (
     <>
       <div>
@@ -18,11 +24,14 @@ export function MainUI() {
       <hr />
 
       <h1>Shows:</h1>
-      <div>
-        {(showsQuery.data as object[])?.map((s) => (
-          <Show show={s} key={(s as { id: number }).id} />
-        ))}
-      </div>
+      {showsQuery.error && <div>Couldn't load shows — try reloading</div>}
+      {showsQuery.data && (
+        <div>
+          {(showsQuery.data as object[])?.map((s) => (
+            <Show show={s} key={(s as { id: number }).id} />
+          ))}
+        </div>
+      )}
     </>
   )
 }
--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
+import { type UseQueryResult } from "@tanstack/react-query"
 
 // Hook: fetches server-side app environment identifier; if run at app startup, also initializes CSRF token
 export function useFetchAppEnvironment(): string | null {
@@ -12,4 +14,18 @@ export function useFetchAppEnvironment(): string | null {
   }, [])
 
   return appEnvironment
+}
+
+// Hook: checks for a React Query query error, logs it, and displays a message
+export function useQueryErrorToast<T>(
+  queryResult: UseQueryResult<T, Error>,
+  message: string,
+): void {
+  useEffect(() => {
+    if (!queryResult.error) {
+      return
+    }
+    console.error(queryResult.error)
+    toast(message)
+  }, [queryResult.error, message])
 }
