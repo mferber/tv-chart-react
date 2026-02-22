45ac9542104854b506073721238b510121eefcef
Matthias Ferber <matthias.ferber@gmail.com>
2026-02-22 11:54:39 -0500

    Added import sorting to eslint on pre-commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import reactX from "eslint-plugin-react-x"
+import reactDom from "eslint-plugin-react-dom"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import { defineConfig, globalIgnores } from "eslint/config"
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -27,4 +35,4 @@ export default defineConfig([
       },
     },
   },
-]);
+])
