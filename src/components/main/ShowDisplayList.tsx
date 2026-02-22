96973ff6b498f35983d2161812c68fff54b60867
Matthias Ferber <matthias.ferber@gmail.com>
2026-02-22 17:11:26 -0500

    Put shows in title order, with a/an/the handling

--- src/components/main/ShowDisplayList.tsx
+++ src/components/main/ShowDisplayList.tsx
@@ -1,10 +1,24 @@
-import { type ShowList } from "../../schemas/schemas"
+import { type Show, type ShowList } from "../../schemas/schemas"
 import { ShowDisplay } from "./ShowDisplay"
+
+function titleSortKey(show: Show): string {
+  return show.title.replace(/^(a|an|the) /i, "").toLowerCase()
+}
+
+function titleSort(shows: ShowList): ShowList {
+  const keyed_shows = shows.map((show) => ({
+    show: show,
+    key: titleSortKey(show),
+  }))
+
+  keyed_shows.sort((a, b) => a.key.localeCompare(b.key))
+  return keyed_shows.map((ks) => ks.show)
+}
 
 export function ShowDisplayList({ shows }: { shows: ShowList }) {
   return (
     <div>
-      {shows.map((show) => (
+      {titleSort(shows).map((show) => (
         <ShowDisplay show={show} key={show.id} />
       ))}
     </div>
