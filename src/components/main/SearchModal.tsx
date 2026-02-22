5a0248d3e7ff69411f93d2b06f05a6a773c239a6
Matthias Ferber <matthias.ferber@gmail.com>
2026-02-22 12:15:02 -0500

    Fixed internal scrolling in search modal

--- src/components/main/SearchModal.tsx
+++ src/components/main/SearchModal.tsx
@@ -11,7 +11,7 @@ export function SearchModal({
     <Modal
       isOpen={isOpen}
       overlayClassName="fixed top-0 right-0 bottom-0 left-0 bg-black/25"
-      className="absolute top-8 right-8 bottom-8 left-8 p-4 border-4 rounded-xl bg-white outline-0"
+      className="absolute top-8 right-8 bottom-8 left-8 p-4 border-4 rounded-xl bg-white outline-0 overflow-auto"
     >
       <div className="text-right">
         <a href="#" onClick={() => close()}>
