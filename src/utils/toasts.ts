fd62c7a644ae8f361b58711a1da00008ad8a7045
Matthias Ferber <matthias.ferber@gmail.com>
2026-03-20 19:00:02 -0400

    Styled toasts more like drop-down menus

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,9 +1,19 @@
 import toast from "react-hot-toast"
 
 export function infoToast(message: string) {
-  toast(message, { position: "top-center", duration: 1000 })
+  toast(message, {
+    position: "top-center",
+    duration: 1000,
+    className:
+      "!text-lg !bg-gray-200 border !rounded-lg !shadow-gray-500 !shadow-lg",
+  })
 }
 
 export function errorToast(message: string) {
-  toast(message, { position: "top-right", duration: 5000 })
+  toast(message, {
+    position: "top-right",
+    duration: 2000,
+    className:
+      "!text-lg !bg-gray-200 border !rounded-lg !shadow-gray-500 !shadow-lg",
+  })
 }
